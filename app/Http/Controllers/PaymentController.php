7ca9d48b245e0eedcfe8347b8e79dce515bbb3a9
doru87 <hurricane.1987@yahoo.com>
2023-10-29 22:48:38 +0200

    newPayment function

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -45,6 +45,6 @@ class PaymentController extends Controller
     // }
 
     public function newPayment(){
-        console.log('It is fucking wrong again and again');
+        console.log('It is all ok');
     }
 }
