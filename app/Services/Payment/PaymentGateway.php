3b2193c26b30c4851345d004ee8b03f2274c0a7c
doru87 <hurricane.1987@yahoo.com>
2023-10-29 17:34:14 +0200

    Constructor commented

--- app/Services/Payment/PaymentGateway.php
+++ app/Services/Payment/PaymentGateway.php
@@ -7,14 +7,14 @@ class PaymentGateway {
 
     private $apiKey;
 
-    public function __construct($apiKey) {
-        $this->apiKey = $apiKey;
-    }
+//    public function __construct($apiKey) {
+//        $this->apiKey = $apiKey;
+//    }
 
     public function charge($amount, $creditCardToken) {
         $stripeService = new StripeService($this->apiKey);
         $response = $stripeService->makePayment($amount, $creditCardToken);
-        
+
         if ($response->success) {
             return true;
         } else {
